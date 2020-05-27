12a79fa679a84ec5ff65d4f8664e3e9430b9cfb2
Harshil Patel <harshil4076@gmail.com>
2020-05-27 19:13:19 -0400

    deleted gitignore

---
+++ build/precache-manifest.a45341eaf08fc160262cc9c0500d59ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5d920265f67577ffcbc8a5f7c5280d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0ddfa302c5d5e813b3",
+    "url": "/static/css/main.6a1da020.chunk.css"
+  },
+  {
+    "revision": "eceb938c336632dfcf03",
+    "url": "/static/js/2.57719259.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57719259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0ddfa302c5d5e813b3",
+    "url": "/static/js/main.a21c7c8c.chunk.js"
+  },
+  {
+    "revision": "9c4189de68529a73df99",
+    "url": "/static/js/runtime-main.65fc1de4.js"
+  }
+]);
